8de89c71f61db479d8c204e49d1379d92bf9785d
HEX <123macanic@naver.com>
2025-06-25 19:53:33 +0900

    feat: gradle 및 환경설정 파일

---
+++ src/main/java/org/inhahackers/optmo_user_be/OptmoUserBeApplication.java
@@ -0,0 +1,11 @@
+package org.inhahackers.optmo_user_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptmoUserBeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OptmoUserBeApplication.class, args);
+    }
+}
---
+++ src/test/java/org/inhahackers/optmo_user_be/OptmoUserBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inhahackers.optmo_user_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptmoUserBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
