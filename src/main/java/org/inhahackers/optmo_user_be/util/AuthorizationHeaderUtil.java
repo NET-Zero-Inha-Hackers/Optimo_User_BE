8628f3460f44e5f3b920a556ae03739341884026
HEX <123macanic@naver.com>
2025-06-25 19:30:11 +0900

    feat: API 요청용 WebClientConfig 및 token추출 Util 클래스(AuthorizationHeaderUtil) 구현

---
+++ src/main/java/org/inhahackers/optmo_user_be/util/AuthorizationHeaderUtil.java
@@ -0,0 +1,18 @@
+package org.inhahackers.optmo_user_be.util;
+
+import org.inhahackers.optmo_user_be.exception.InvalidAuthorizationHeaderException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+public class AuthorizationHeaderUtil {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    public static String extractToken(ServerRequest.Headers headers) {
+        String authHeader = headers.firstHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith(BEARER_PREFIX)) {
+            throw new InvalidAuthorizationHeaderException("Missing or invalid Authorization header");
+        }
+        return authHeader.substring(BEARER_PREFIX.length());
+    }
+}
